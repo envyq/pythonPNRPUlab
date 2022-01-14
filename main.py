f044ea93e6e0a663dc02bf1dc3ea53383aa637d7
envyq <i.putin@greendatasoft.ru>
2022-01-14 23:01:36 +0500

    feat: creating a blank to draw

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ cv2.imshow('default image', defaultImage)
 # hsv
 lowSide = numpy.array([49, 41, 218])
 upSide = numpy.array([100, 100, 255])
-
+# creating a blank to draw
+result = numpy.copy(defaultImage) * 0
 mask = cv2.inRange(defaultImage, lowSide, upSide)
-cv2.imshow('mask iamge', mask)
+cv2.imshow('mask image', mask)
+cv2.imshow('zero ia=mge', result)
 
 cv2.waitKey(0)
