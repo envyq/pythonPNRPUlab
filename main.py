faa10a0fd73d7641e357c2da0259089c115cde5e
Nataliya589 <you@example.com>
2022-01-15 01:22:57 +0500

    add import math.  calculating the equation of a straight line and drawing a line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy
+import math
 
 defaultImage = cv2.imread('template.png')
 cv2.imshow('default image', defaultImage)
@@ -19,5 +20,23 @@ min_line_length = 50  # минимальное количество пиксел
 max_line_gap = 20  # максимальный промежуток в пикселях между соединяемыми сегментами линии
 image_lines = cv2.HoughLinesP(mask, rho, theta, threshold, numpy.array([]), min_line_length, max_line_gap)
 print('image_lines', image_lines)
+
+_length, _x1, _y1, _x2, _y2 = -1, -1, -1, -1, -1
+
+for image_line in image_lines:
+    for x1, y1, x2, y2 in image_line:
+        length = math.sqrt(math.pow(int(x2) - int(x1), 2) + math.pow(int(y2) - int(y1), 2))
+        if length > _length: _length, _x1, _y1, _x2, _y2 = length, x1, y1, x2, y2
+
+cv2.line(result, (_x1, _y1), (_x2, _y2), (255, 0, 0), 5)
+destination = cv2.resize(result, (500, 500))
+
+# Вычисляем уравнение прямой
+k = (_y1 - _y2) / (_x1 - _x2)
+b = _y2 - k * _x2
+
+print(f"Line equation: y = {-1 * round(k, 2)}*x + {int(b)}")
+
+cv2.imshow('destination', destination)
 
 cv2.waitKey(0)
